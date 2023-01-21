1edd1901522f71cb8f2dd033f78d58b57cdd4484
Wei Li <weili.mail@gmail.com>
2023-01-21 08:03:21 -0500

    Switch to vue composition api

--- src/components/marathon.vue
+++ src/components/marathon.vue
@@ -1,78 +1,72 @@
-<script>
-import  axios  from 'axios'
+<script setup>
+import { ref, onMounted, onBeforeMount } from 'vue'
+import axios from 'axios'
 
-export default {
-    data: () => ({
-            marathons: [],
-            currentMarathon: [],
-            quotes: [],
-            randomQuote: "",
-            totalPages: 8,
-            numberPerPage: 10,
-            showResult: true
-    }),
+const marathons = ref([])
+const currentMarathon = ref([])
+const quotes = ref([])
+const randomQuote = ref('')
+const totalPages = ref(8)
+const numberPerPage = ref(10)
+const showResult = ref(true)
 
-    created: function () {
-        this.loadQuotes();
-        let randomPage = Math.floor(Math.random() * this.totalPages + 1);
-        let randomMarathon = (randomPage - 1) * this.numberPerPage + Math.floor(Math.random() * this.numberPerPage + 1);
-        this.loadMarathons(randomPage);
-        this.loadMarathon(randomMarathon);
-    },
+// onMounted(() => {
+onBeforeMount( () => {
+    loadQuotes();
+    let randomPage = Math.floor(Math.random() * totalPages.value + 1);
+    let randomMarathon = (randomPage - 1) * numberPerPage.value + Math.floor(Math.random() * numberPerPage.value + 1);
+    loadMarathons(randomPage);
+    loadMarathon(randomMarathon);
+})
 
-    methods: {
-        loadQuotes() {
-            let url = 'assets/content/data/quotes.json';
-            axios.get(url).then(resp => {
-                this.quotes = resp.data
-                this.randomQuote = this.quotes[Math.floor(Math.random() * this.quotes.length)];
-            }).catch(e => {
-                console.log(e)
-            });
-        },
+function loadQuotes() {
+    let url = 'assets/content/data/quotes.json';
+    axios.get(url).then(resp => {
+        quotes.value = resp.data
+        randomQuote.value = quotes.value[Math.floor(Math.random() * quotes.value.length)];
+    }).catch(e => {
+        console.log(e)
+    });
+}
 
-        loadMarathons(page) {
-            // let p = page > 0 ? page : 1; 
-            let url = 'assets/content/data/marathons' + (page > 0 ? page : 1) + '.json';
-            axios.get(url).then(response => {
-                this.marathons = response.data
-            });
-        },
+function loadMarathons(page) {
+    let url = 'assets/content/data/marathons' + (page > 0 ? page : 1) + '.json';
+    axios.get(url).then(response => {
+        marathons.value = response.data
+    });
+}
 
-        loadAll() {
-            let a = [];
-            let self = this;
-            for (let i = 1; i < 11; i++) {
-                let url = 'assets/content/data/marathons' + i + '.json';
-                axios.get(url).then( response => {
-                        if (i == 1) {
-                            a = response.data;
-                            a.hasPrevious = true;
-                            a.hasNext = false;
-                            self.loadMarathon(1);
-                        } else {
-                            a.data = a.data.concat(response.data.data);
-                        }
-                    this.marathons = a;
-                });
+function loadAll() {
+    let a = [];
+    for (let i = 1; i < 11; i++) {
+        let url = 'assets/content/data/marathons' + i + '.json';
+        axios.get(url).then(response => {
+            if (i == 1) {
+                a = response.data;
+                a.hasPrevious = true;
+                a.hasNext = false;
+                loadMarathon(1);
+            } else {
+                a.data = a.data.concat(response.data.data);
             }
-        },
-
-        loadMarathon(id) {
-            this.showResult = false;
-            let url = 'assets/content/data/marathon' + id.toString() + '.json';
-            axios.get(url).then(response => {
-                this.currentMarathon = response.data
-            });
-            this.randomQuote = this.quotes[Math.floor(Math.random() * this.quotes.length)];
-            setTimeout(() => this.showResult = true, 500);
-        },
-
-        loadPage(page) {
-            this.loadMarathons(page);
-            this.loadMarathon((page - 1) * this.numberPerPage + 1);
-        },
+            marathons.value = a;
+        });
     }
+}
+
+function loadMarathon(id) {
+    showResult.value = false;
+    let url = 'assets/content/data/marathon' + id.toString() + '.json';
+    axios.get(url).then(response => {
+        currentMarathon.value = response.data
+    });
+    randomQuote.value = quotes.value[Math.floor(Math.random() * quotes.value.length)];
+    setTimeout(() => showResult.value = true, 500);
+}
+
+function loadPage(page) {
+    loadMarathons(page);
+    loadMarathon((page - 1) * numberPerPage.value + 1);
 }
 </script>
 
@@ -89,7 +83,8 @@ export default {
                                     <a class="page-link" href="#" @click="loadPage(1)">10</a>
                                 </li>
                                 <li class="page-item" :class="{ disabled: !marathons.hasPrevious }">
-                                    <a class="page-link" href="#" @click="loadPage(marathons.pageNumber - 1)">Previous</a>
+                                    <a class="page-link" href="#"
+                                        @click="loadPage(marathons.pageNumber - 1)">Previous</a>
                                 </li>
                                 <li class="page-item" :class="{ disabled: !marathons.hasNext }">
                                     <a class="page-link" href="#" @click="loadPage(marathons.pageNumber + 1)">Next</a>
@@ -103,41 +98,49 @@ export default {
                             </ul>
                         </nav>
                     </div>
- 
+
                     <div>
                         <table class="table table-hover table-borderless">
                             <tr v-for="marathon in marathons.data">
                                 <td>
-                                    <span class="fa fa-star-o font-italic"><i style="font-size:18px;color:blue">{{marathon.id}}</i>
+                                    <span class="fa fa-star-o font-italic"><i
+                                            style="font-size:18px;color:blue">{{ marathon.id }}</i>
                                     </span>
-                                    - <a href="#" @click="loadMarathon(marathon.id)" :class="{'bg-info' : marathon.id == currentMarathon.id}">{{ marathon.name }}</a>
+                                    - <a href="#" @click="loadMarathon(marathon.id)"
+                                        :class="{ 'bg-info': marathon.id == currentMarathon.id }">{{ marathon.name }}</a>
                                 </td>
                             </tr>
                         </table>
                     </div>
                 </div>
-                <div class="col-md-6" >
+                <div class="col-md-6">
                     <transition name="result" mode="out-in">
-                    <div v-if="showResult && currentMarathon.id > 0 " >
-                    <h5>{{currentMarathon.name}}</h5>
-                    <ul class="list-unstyled list-group w-75 mt-4">
-                        <li list-group-item><i class="fa-solid fa-calendar-days"></i><span class="mx-4 mb-2 badge bg-secondary ">{{currentMarathon.date}}</span></li>
-                        <li list-group-item><i class="fa-solid fa-location-pin"></i><span class="mx-4 mb-2 badge bg-secondary ">{{currentMarathon.location}}</span></li>
-                        <li list-group-item><i class="fa-solid fa-person-running"></i><span class="mx-4 mb-2 badge bg-secondary ">{{currentMarathon.distance}}</span></li>
-                        <li list-group-item><i class="fa-regular fa-clock"></i><span class="mx-4 mb-2 badge bg-secondary ">{{currentMarathon.time}}</span></li>
-                        <li list-group-item><i class="fa-solid fa-bolt"></i><span class="mx-4 mb-2 badge bg-secondary ">{{currentMarathon.pace}}</span></li>
-                    </ul>
-                    <div class="mt-4">
-                        <img class="rounded" v-bind:src="'assets/content/img/' + currentMarathon.picture" v-bind:alt="currentMarathon.name" height="200">
-                    </div>
+                        <div v-if="showResult && currentMarathon.id > 0">
+                            <h5>{{ currentMarathon.name }}</h5>
+                            <ul class="list-unstyled list-group w-75 mt-4">
+                                <li list-group-item><i class="fa-solid fa-calendar-days"></i><span
+                                        class="mx-4 mb-2 badge bg-secondary ">{{ currentMarathon.date }}</span></li>
+                                <li list-group-item><i class="fa-solid fa-location-pin"></i><span
+                                        class="mx-4 mb-2 badge bg-secondary ">{{ currentMarathon.location }}</span></li>
+                                <li list-group-item><i class="fa-solid fa-person-running"></i><span
+                                        class="mx-4 mb-2 badge bg-secondary ">{{ currentMarathon.distance }}</span></li>
+                                <li list-group-item><i class="fa-regular fa-clock"></i><span
+                                        class="mx-4 mb-2 badge bg-secondary ">{{ currentMarathon.time }}</span></li>
+                                <li list-group-item><i class="fa-solid fa-bolt"></i><span
+                                        class="mx-4 mb-2 badge bg-secondary ">{{ currentMarathon.pace }}</span></li>
+                            </ul>
+                            <div class="mt-4">
+                                <img class="rounded" v-bind:src="'assets/content/img/' + currentMarathon.picture"
+                                    v-bind:alt="currentMarathon.name" height="200">
+                            </div>
 
-                    </div>
+                        </div>
                     </transition>
                 </div>
             </div>
             <div class="container  mt-4 rounded">
                 <blockquote class="blockquote text-center font-weight-bold font-italic">
-                    <i style="font-size:20px;color:black">"{{randomQuote}}"</i>
+                    <i style="font-size:20px;color:black">"{{ randomQuote }}"</i>
                 </blockquote>
             </div>
         </div>
@@ -147,19 +150,20 @@ export default {
 
 <style>
 .result-enter-from {
-  opacity: 0;
-  transform: translateX(300px);
+    opacity: 0;
+    transform: translateX(300px);
 }
+
 .result-enter-active {
-  transition: all 0.3s ease-out; 
+    transition: all 0.3s ease-out;
 }
+
 .result-leave-to {
-  opacity: 0;
-  transform: translateX(-100px);
+    opacity: 0;
+    transform: translateX(-100px);
 }
+
 .result-leave-active {
-  transition: all 0.3s ease-in; 
+    transition: all 0.3s ease-in;
 }
-
-
 </style>
